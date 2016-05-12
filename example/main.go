e84462888333cce3f57332f9f31ac3893c1ada7f
henderjon <list@box370.net>
2016-05-12 12:38:16 -0500

    better example

--- example/main.go
+++ example/main.go
@@ -4,21 +4,32 @@ import(
 	"log"
 	"github.com/henderjon/shutdown"
 	"time"
+	"sync"
 )
 
 func main(){
 
+	var wg sync.WaitGroup
 	signal := make(shutdown.SignalChan)
 
-	go func(){
+	wg.Add(1)
+	go func(signal shutdown.SignalChan){
 		for {
-			log.Println("Zzzz")
-			time.Sleep(time.Second * 5)
+			select {
+			case <-signal:
+				time.Sleep(time.Second * 3)
+				wg.Done()
+			default:
+				log.Println("Zzzz")
+				time.Sleep(time.Second * 3)
+			}
 		}
-	}()
+	}(signal)
 
 	shutdown.Watch(signal, func() {
 		log.Println("3...2...1...")
+		wg.Wait()
+		log.Println("and done.")
 	})
 
 }
